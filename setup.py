9cc6720bc58e5fad558581c28d375d2f7c620516
mrJeppard <duncmc831@gmail.com>
2017-06-15 15:46:55 -0700

    set up

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hexamaps',
+      version='0.0',
+      description='A collection of methods used in the Tumor Map pipeline.',
+      url='https://github.com/mrJeppard/hexamaps.git',
+      author='Duncan McColl',
+      author_email='duncmc831@gmail.com',
+      license='MIT',
+      packages=['hexamaps'],
+      zip_safe=False)
